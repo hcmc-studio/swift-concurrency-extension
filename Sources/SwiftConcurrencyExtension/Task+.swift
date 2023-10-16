7ea8b4b62a752b6b27e0d1eead0e5df32ef68e5a
Ji-Hwan KIM <nodealdev@gmail.com>
2023-10-16 11:55:50 +0900

    [Script push]

---
+++ Sources/SwiftConcurrencyExtension/Task+.swift
@@ -0,0 +1,84 @@
+//
+//  Task+.swift
+//
+//
+//  Created by Ji-Hwan Kim on 10/16/23.
+//
+
+import Foundation
+
+extension Task {
+    public static func execute(
+        priority: TaskPriority? = nil,
+        fetch: @escaping () async throws -> Void,
+        onReady: @escaping ((any Error)?) -> Void
+    ) -> Task<Void, any Error> {
+        Task<Void, any Error>(priority: priority) {
+            try await operation(fetch: fetch, onReady: { result in
+                switch result {
+                case .success(): onReady(nil)
+                case .failure(let error): onReady(error)
+                }
+            })
+        }
+    }
+    
+    public static func execute(
+        priority: TaskPriority? = nil,
+        fetch: @escaping () async throws -> Void,
+        onSuccess: @escaping () -> Void,
+        onFailure: @escaping (any Error) -> Void
+    ) -> Task<Void, any Error> {
+        Task<Void, any Error>(priority: priority) {
+            try await operation(fetch: fetch, onReady: { result in
+                switch result {
+                case .success(): onSuccess()
+                case .failure(let error): onFailure(error)
+                }
+            })
+        }
+    }
+    
+    public static func execute<FetchResult: Sendable>(
+        priority: TaskPriority? = nil,
+        fetch: @escaping () async throws -> FetchResult,
+        onReady: @escaping (Result<FetchResult, Error>) -> Void
+    ) -> Task<FetchResult, any Error> {
+        Task<FetchResult, any Error>(priority: priority) {
+            try await operation(fetch: fetch, onReady: onReady)
+        }
+    }
+    
+    public static func execute<FetchResult: Sendable>(
+        priority: TaskPriority? = nil,
+        fetch: @escaping () async throws -> FetchResult,
+        onSuccess: @escaping (FetchResult) -> Void,
+        onFailure: @escaping (any Error) -> Void
+    ) -> Task<FetchResult, any Error> {
+        Task<FetchResult, any Error>(priority: priority) {
+            try await operation(fetch: fetch, onReady: { result in
+                switch result {
+                case .success(let result): onSuccess(result)
+                case .failure(let error): onFailure(error)
+                }
+            })
+        }
+    }
+    
+    private static func operation<FetchResult: Sendable>(
+        priority: TaskPriority? = nil,
+        fetch: () async throws -> FetchResult,
+        onReady: (Result<FetchResult, Error>) -> Void
+    ) async rethrows -> FetchResult {
+        do {
+            let fetchResult = try await fetch()
+            await MainActor.run { onReady(.success(fetchResult)) }
+            
+            return fetchResult
+        } catch let error {
+            await MainActor.run { onReady(.failure(error)) }
+            
+            throw error
+        }
+    }
+}
