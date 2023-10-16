0118796e51755f0e96fd0f7e86c6b142a511357e
Ji-Hwan KIM <nodealdev@gmail.com>
2023-10-16 21:51:12 +0900

    [Script push]

---
+++ Sources/SwiftConcurrencyExtension/Sequence+.swift
@@ -0,0 +1,70 @@
+//
+//  Sequence+.swift
+//
+//
+//  Created by Ji-Hwan Kim on 10/16/23.
+//
+
+import Foundation
+
+extension Sequence {
+    public func mapSerial<Result>(
+        transform: (Element) async throws -> Result
+    ) async rethrows -> [Result] {
+        var arr = [Result]()
+        for element in self {
+            arr.append(try await transform(element))
+        }
+        
+        return arr
+    }
+    
+    public func mapParallel<Result>(
+        priority: TaskPriority? = nil,
+        transform: @escaping (Element) async throws -> Result
+    ) async rethrows -> [Result] {
+        let tasks = map { element in
+            Task<Result, any Error>(priority: priority) {
+                try await transform(element)
+            }
+        }
+        return try await tasks.mapSerial { task in
+            try await task.value
+        }
+    }
+    
+    public func filterSerial(
+        predicate: (Element) async throws -> Bool
+    ) async rethrows -> [Element] {
+        var arr = [Element]()
+        for element in self {
+            if try await predicate(element) {
+                arr.append(element)
+            }
+        }
+        
+        return arr
+    }
+    
+    public func filterParallel(
+        priority: TaskPriority? = nil,
+        predicate: @escaping (Element) async throws -> Bool
+    ) async rethrows -> [Element] {
+        var tasks = [Task<(Element, Bool), any Error>]()
+        for element in self {
+            tasks.append(Task<(Element, Bool), any Error>(priority: priority) {
+                (element, try await predicate(element))
+            })
+        }
+        
+        var arr = [Element]()
+        for task in tasks {
+            let (element, result) = try await { try await task.value }()
+            if result {
+                arr.append(element)
+            }
+        }
+        
+        return arr
+    }
+}
